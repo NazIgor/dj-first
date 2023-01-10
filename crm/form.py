ff19957adad5072e1e3606dabffe98d64e1ff927
Igor Nazarenko <nazarenko.epson@gmail.com>
2023-01-10 15:57:50 +0600

    add html templates

--- crm/form.py
+++ crm/form.py
@@ -1,5 +1,7 @@
 from django import forms
 
 class OrderForm (forms.Form):
-    name=forms.CharField(max_length=200)
-    phone=forms.CharField(max_length=30)
+    name=forms.CharField(max_length=200, label='Ваше имя')
+    phone=forms.CharField(max_length=30, label='Ваше номер')
+    
+    
